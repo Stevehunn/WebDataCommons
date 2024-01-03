7ad70006baab5d2ad65fe6ab9e84013715010ad2
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2024-01-03 23:41:15 +0100

    Add select row and show plot corresponding

--- testplot.py
+++ testplot.py
@@ -12,6 +12,8 @@ from upsetplot import plot, from_memberships, UpSet
 
 def content_testplot():
     st.write("genere plot")
+    row = ["10", "15", "20", "30", "40", "50", "all"]
+    select = st.selectbox("Select the number of row", row, index=0)
     with open('./newData/after/Action.json', 'r') as f:
         data_dict = json.load(f)
         # st.write(data_dict)
@@ -27,15 +29,50 @@ def content_testplot():
     #st.write([[e in item["pset"] for item in data_dict] for e in list_labels])
     #df = pd.DataFrame([[e in item["pset"] for e in list_labels] for item in data_dict], columns=list_labels)
     df = pd.DataFrame([[e in item["pset"] for e in list_labels] + [item["count"]] for item in data_dict], columns=list_labels+["count"])
-
     count_column = df.pop("count")
     #st.write(df)
-    df_up = df.groupby(list_labels).value_counts()
-    for i in range(len(df_up)):
-        df_up[i] = count_column[i]
+    if select !="all":
+        select = int(select)
+        df_reduce = df.head(select)
+        column_to_remove = list()
+        for column in df_reduce.columns:
+            canary = False
+            for item in df_reduce[column]:
+                canary |= item
+            if not canary:
+                column_to_remove.append(column)
+        #print(column_to_remove)
+        for column in column_to_remove:
+            df_reduce.pop(column)
+            list_labels.remove(column)
+        #st.write(df_reduce)
 
-    st.write(df_up)
-    plot(df_up, orientation='horizontal')
-    plt.savefig("lama.svg")
-    st.image("lama.svg")
-    st.write("finished.")
+        #----------Save and Show n row of the dataframe------------------
+        st.write("Show the number of rows select.")
+        df_up = df_reduce.groupby(list_labels).value_counts()
+
+        for i in range(len(df_up)):
+            df_up[i] = count_column[i]
+
+        #st.write(df_up)
+
+        plot(df_up, orientation='horizontal')
+        plt.savefig("lama.svg")
+        st.image("lama.svg")
+
+    if select == "all":
+        # ----------Save and Show all the dataframe------------------
+        st.write("Show all rows available.")
+        df_up_all = df.groupby(list_labels).value_counts()
+
+        for i in range(len(df_up_all)):
+            df_up_all[i] = count_column[i]
+
+        #st.write(df_up_all)
+
+        plot(df_up_all, orientation='horizontal')
+        plt.savefig("lamacomplet.svg")
+        st.image("lamacomplet.svg")
+        st.write("finished.")
+
+
