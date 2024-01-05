0a9c05298f4a2b85ba2dd1ec01017b5e3c8439b9
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2024-01-05 18:50:46 +0100

    Add parse for all platform

--- main.py
+++ main.py
@@ -4,77 +4,47 @@ from streamlit_option_menu import option_menu
 # Import Content Page
 from comparison import content_comparison
 from dataCount import dataCount
-from parse import parseWindow, parseMac, parseAfterJsonAvailableMac, parseAfterJsonAvailableWindow, \
-    parseBeforeJsonAvailableMac, parseBeforeJsonAvailableWindow
+from parse import get_plot_name, get_name_from_json
 from welcome import content_welcome
 from year2022 import content_2022
 from year2023 import content_2023
 
-# Custom CSS
-custom_css = """
-<style>
-    /* Your custom CSS goes here */
-   [data-testid=stImage]{
-            text-align: center;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 100%;
-    }
-    .st-emotion-cache-1y4p8pa {
-        max-width: 95%
-    }
-</style>
-"""
 
-# Init data
-target_classes = []
-target_classes_available_after = []
-target_classes_available_before = []
-
-# Parse Data from target classes
-
-# parseWindow(target_classes)
-# parseMac(target_classes)
-
-# Parse Json Data from target classes
-
-# parseAfterJsonAvailableWindow(target_classes_available_after)
-# parseAfterJsonAvailableMac(target_classes_available_after)
-
-# parseBeforeJsonAvailableWindow(target_classes_available_before)
-# parseBeforeJsonAvailableMac(target_classes_available_before)
-
-
-version_mac = False
-if version_mac:
-    parseMac(target_classes)
-    parseAfterJsonAvailableMac(target_classes_available_after)
-    parseBeforeJsonAvailableMac(target_classes_available_before)
-else:
-    parseWindow(target_classes)
-    parseAfterJsonAvailableWindow(target_classes_available_after)
-    parseBeforeJsonAvailableWindow(target_classes_available_before)
-
-# Data before
-
-data_plotly_sunburst_before = dataCount(False, True)
-data_plotly_treemap_before = dataCount(True, True)
-
-# Data after
-
-data_plotly_sunburst_after = dataCount(False, False)
-data_plotly_treemap_after = dataCount(True, False)
-
-
-# Define content show, sidebar
 def main():
-    content_sidebar()
-    return None
+    # Custom CSS
+    custom_css = """
+    <style>
+        /* Your custom CSS goes here */
+       [data-testid=stImage]{
+                text-align: center;
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 100%;
+        }
+        .st-emotion-cache-1y4p8pa {
+            max-width: 95%
+        }
+    </style>
+    """
 
+    # Init data
+    target_classes = []
+    target_classes_available_after = []
+    target_classes_available_before = []
 
-# Side Content
-def content_sidebar():
+    get_plot_name(target_classes)
+    get_name_from_json(target_classes_available_after, "after")
+    get_name_from_json(target_classes_available_before, "before")
+
+    # Data before
+    data_plotly_sunburst_before = dataCount(False, True)
+    data_plotly_treemap_before = dataCount(True, True)
+
+    # Data after
+    data_plotly_sunburst_after = dataCount(False, False)
+    data_plotly_treemap_after = dataCount(True, False)
+
     # Display custom CSS
     st.markdown(custom_css, unsafe_allow_html=True)
 
--- parse.py
+++ parse.py
@@ -1,150 +1,30 @@
 import glob
-import re
 
 
-# Version Window
-def parseWindow(target_classes):
+def get_plot_name(target_classes: list):
     for file in glob.glob("assets/plots/*.svg"):
+        filename = file.replace('\\', '/')
         # Utilisez split pour séparer le chemin du fichier
-        parts = file.split("/")
-
-        # Extrait le premier élément après "assets"
-        fname = parts[1]  # parts[0] est "assets", parts[1] est "plots"
-
-        parts2 = fname.split("plots")
-        # Extrait le premier élément après "plots"
-        newFname = parts2[1]
-
-        # Utiliser split pour séparer la chaîne en fonction de "\"
-        parts = newFname.split("\\")
-
-        # Concaténer les parties avec "schema:"
-        newFname = "".join(parts[1:])
-
+        parts = filename.split("/")
         # Supprime l'extension ".svg"
-        cname = newFname.split("_plot.svg")[0]
-
+        cname = parts[2].replace("_plot.svg", "")
         # Ajoute à la liste avec le préfixe "schema:"
         target_classes.append(f"schema:{cname}")
 
 
-# Version Mac
-def parseMac(target_classes):
-    for file in glob.glob("assets/plots/*.svg"):
+def get_name_from_json(target_classes_available: list, subdir: str):
+    for file in glob.glob(f"newData/{subdir}/*.json"):
+        filename = file.replace('\\', '/')
         # Utilisez split pour séparer le chemin du fichier
-        parts = file.split("/")
-
-        fname = parts[2]
-        parts2 = fname.split(".")
-        # Extrait le premier élément après "plots"
-        newFname = parts2[0]
-
-        cname = newFname.split("_plot")[0]
-        if cname != "Intangible":
-            target_classes.append(f"schema:{cname}")
-
-
-def parseAfterJsonAvailableWindow(target_classes_available):
-    for file in glob.glob("newData/after/*.json"):
-        # Utilisez split pour séparer le chemin du fichier
-        parts = file.split("/")
-        # print(parts)
-        # Extrait le premier élément après "assets"
-        fname = parts[1]  # parts[0] est "assets", parts[1] est "plots"
-        # print(fname)
-        # print("Fname")
-        parts2 = fname.split("plots")
-        # print(parts2)
-        # print("parts2")
-        # Extrait le premier élément après "plots"
-        newFname = parts2[0]
-        # print(newFname)
-        # Utiliser split pour séparer la chaîne en fonction de "\"
-        parts = newFname.split("\\")
-        # print(parts)
-        # Concaténer les parties avec "schema:"
-        newFname = "".join(parts[1:])
-        # print(newFname)
-        # Supprime l'extension ".svg"
-        cname = newFname.split(".json")[0]
-
+        parts = filename.split("/")
+        # Supprime l'extension ".json"
+        cname = parts[2].replace(".json", "")
         # Ajoute à la liste avec le préfixe "schema:"
         target_classes_available.append(f"schema:{cname}")
-        # print("target_classes_available")
-        # print(target_classes_available)
 
 
-def parseAfterJsonAvailableMac(target_classes_available):
-    for file in glob.glob("newData/after/*.json"):
-        # Utilisez split pour séparer le chemin du fichier
-        parts = file.split("/")
-
-        fname = parts[2]
-        parts2 = fname.split(".")
-        # Extrait le premier élément après "plots"
-        newFname = parts2[0]
-
-        cname = newFname.split("_plot")[0]
-        if cname != "Intangible":
-            target_classes_available.append(f"schema:{cname}")
-
-
-def parseBeforeJsonAvailableWindow(target_classes_available):
-    for file in glob.glob("newData/before/*.json"):
-        # Utilisez split pour séparer le chemin du fichier
-        parts = file.split("/")
-        # print(parts)
-        # Extrait le premier élément après "assets"
-        fname = parts[1]  # parts[0] est "assets", parts[1] est "plots"
-        # print(fname)
-        # print("Fname")
-        parts2 = fname.split("plots")
-        # print(parts2)
-        # print("parts2")
-        # Extrait le premier élément après "plots"
-        newFname = parts2[0]
-        # print(newFname)
-        # Utiliser split pour séparer la chaîne en fonction de "\"
-        parts = newFname.split("\\")
-        # print(parts)
-        # Concaténer les parties avec "schema:"
-        newFname = "".join(parts[1:])
-        # print(newFname)
-        # Supprime l'extension ".svg"
-        cname = newFname.split(".json")[0]
-
-        # Ajoute à la liste avec le préfixe "schema:"
-        target_classes_available.append(f"schema:{cname}")
-        # print("target_classes_available")
-        # print(target_classes_available)
-
-
-def parseBeforeJsonAvailableMac(target_classes_available):
-    for file in glob.glob("newData/before/*.json"):
-        # Utilisez split pour séparer le chemin du fichier
-        parts = file.split("/")
-
-        fname = parts[2]
-        parts2 = fname.split(".")
-        # Extrait le premier élément après "plots"
-        newFname = parts2[0]
-
-        cname = newFname.split("_plot")[0]
-        if cname != "Intangible":
-            target_classes_available.append(f"schema:{cname}")
-
-
-def extraire_contenu_apres_backslash(ma_ligne):
-    # Regex pour supprimer tout le contenu avant le dernier caractère '\'
-    nouveau_contenu = re.sub(r'^.*\\', '', ma_ligne)
-    return nouveau_contenu
-
-
-def getCheminForImage(nomfichier):
-    regexSelect = extraire_contenu_apres_backslash(nomfichier)
+def getCheminForImage(nomfichier: str) -> str:
     # Utiliser split pour séparer la chaîne en fonction de ":"
-    parts = regexSelect.split(":")
-
+    filename = nomfichier.split(":")[1]
     # Concaténer les parties avec le format souhaité
-    result = f"assets/plots/{parts[1]}_plot.svg"
-    return result
+    return f"assets/plots/{filename}_plot.svg"
